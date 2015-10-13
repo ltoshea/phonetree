655bcd41afd670982765791e2220ac4fce0d8902
ltoshea <ltoshea+github@gmail.com>
2015-10-13 21:43:09 +0100

    first commit

---
+++ task1.py
@@ -0,0 +1,40 @@
+#Task1.py
+# (question,response if yes, response if no)
+questions = {
+	1: ('Is your phone turning on?',4,2),
+	2: ('Does your phone charge when plugged into the wall?',4,3),
+	3: ('Are you using the official charger that came with your phone?',),
+	4: ('Are you having display problems?',7,5,),
+	5: ('Does the screen appear to be cracked or have black splotches beneath the screen?','',''),
+	6: ('Is your battery removable?','',''),
+	7: ('Are you having a problem with a specific app on your phone?','fuck off','2nd value'),
+}
+
+def fetchanswer(qno,index):
+	if isinstance(questions[qno][index],int):
+		#print 'here'
+		return questions[questions[qno][index]][0]
+	else:
+		#print 'here2'
+		return questions[qno][index]
+
+
+
+print questions[1][0]
+a = raw_input()
+if a=='yes':
+	print fetchanswer(1,1)
+else:
+	print fetchanswer(1,2)
+
+# x = 1
+# while isinstance(x,int):
+#     print questions[x][0]
+#     a = 'yes'	
+#     if a is "yes": 
+#         x = questions[x][1]
+#         print x 
+#     if a is "no": 
+#         x = questions[x][2] 
+#         print x
+
